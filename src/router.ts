6aec12c0405270782a70a46986fb2b47075ea5c8
Tim <tim103@pm.me>
2024-12-27 17:06:06 +1100

    Removed about page.

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
 
 import Home from "./pages/index.vue";
-import About from "./pages/about.vue";
 
 const routes: Array<RouteRecordRaw> = [
   { path: "/", component: Home },
-  { path: "/about", component: About },
 ];
 
 const router = createRouter({
